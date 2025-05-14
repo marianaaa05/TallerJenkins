bef25ee54f29463ceb6c9573a18cbbcf924554bf
marianaaa05 <nanaarredondo220@gmail.com>
2025-05-14 10:29:31 -0500

    prueba automatizada

--- holaMundo.py
+++ holaMundo.py
@@ -1,1 +1,10 @@
-print("Hola Mundo desde Python")
+# print("Hola Mundo desde Python")
+
+
+
+
+def obtener_saludo(nombre):
+    return f"Hola {nombre}, bienvenidas a Jenkins!"
+
+if __name__ == "__main__":
+    print(obtener_saludo("Mariana y carolina"))
--- test_hola_mundo.py
+++ test_hola_mundo.py
@@ -1,12 +1,27 @@
 # Simula una "prueba" verificando que el saludo sea correcto
 
-def prueba():
-    mensaje = "Hola Mundo desde Python"
-    if "Hola" in mensaje:
-        print("Este es un hola desde Python en Jenkins")
-        exit(0)
-    else:
-        print("Prueba fallida: ha ocurrido un error")
-        exit(1)
+# def prueba():
+#     mensaje = "Hola Mundo desde Python"
+#     if "Hola" in mensaje:
+#         print("Este es un hola desde Python en Jenkins")
+#         exit(0)
+#     else:
+#         print("Prueba fallida: ha ocurrido un error")
+#         exit(1)
 
-prueba()
+# prueba()
+
+
+
+
+from holaMundo import obtener_saludo
+
+def test_saludo_correcto():
+    nombre = "Carolina"
+    saludo = obtener_saludo(nombre)
+    
+    if saludo == f"Hola {nombre}, bienvenido a Jenkins!":
+        print("✅ Prueba exitosa: El saludo es correcto.")
+    else:
+        print("❌ Prueba fallida: El saludo no es el esperado.")
+        exit(1)  # Esto hace que Jenkins detecte el fallo
